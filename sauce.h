732bccaa0658fd6eb78824047f9a72b66ebc98ba
yungKram <markartyukh@gmail.com>
2021-09-15 08:43:07 +0300

    .

--- sauce.h
+++ sauce.h
@@ -1,14 +1,21 @@
 #ifndef PHILOSOPHERS_SAUCE_H
 #define PHILOSOPHERS_SAUCE_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <sys/time.h>
+#include <unistd.h>
 
 typedef struct  s_params {
     int         number_of_philosophers;
-    float       time_to_die;
-    float       time_to_eat;
-    float       time_to_sleep;
+    int       time_to_die;
+    int       time_to_eat;
+    int       time_to_sleep;
     int         optional_argument_specified;
     int         number_of_times_each_philosopher_must_eat;
-    int         forks[];
+    int         *forks;
+    int			philosopher_owned;
 }               t_params;
 
 #endif
